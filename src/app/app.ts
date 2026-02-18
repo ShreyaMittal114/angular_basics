2ff23fe614f306b2d522f967b99c3decf80be166
Mittal <shmittal@ciena.com>
2026-02-18 23:18:11 +0530

    updated'

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,17 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  // template :`<h1> inline temp{{name}} </h1> `,  inline temp
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-basics');
+  name = "Shreya"
+  getname(){
+     return this.name
+  }
+
+   getsum(a:number,b:number){
+     return a+b;
+  }
 }
